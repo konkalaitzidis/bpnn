13cb78a9575124474267b96e01bf82a3df8e5de0
konkalaitzidis <kon.kalaitzidis@gmail.com>
2023-04-17 16:41:00 +0200

    making modifications to load multiple calcium videos

--- src/V3/load_calcium_video.py
+++ src/V3/load_calcium_video.py
@@ -12,16 +12,26 @@ def load_video_data(s3_calcium_url):
         fs=fsspec.filesystem("http"),
         cache_storage="nwb-cache",  # Local folder for the cache
     )
-
+            
     with fs.open(s3_calcium_url, "rb") as f:
         with h5py.File(f) as file:
-            video_data = np.array(file["analysis/recording_20211028_181307-PP-BP-MC/data"])
+            analysis_group = file["analysis"]
+            recording_group_name = list(analysis_group.keys())[0]
+            recording_group = analysis_group[recording_group_name]
+            print("working..")
+            video_data = np.array(recording_group["data"][:])
+            return video_data
+
+
+    # with fs.open(s3_calcium_url, "rb") as f:
+    #         with h5py.File(f) as file:
+    #             video_data = np.array(file["analysis/recording_20211028_181307-PP-BP-MC/data"])
 
     end_time = time.time()
     execution_time = end_time - start_time
     hours, remainder = divmod(execution_time, 3600)
     minutes, seconds = divmod(remainder, 60)
-
+ 
     print(f"Execution time: {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds")
     
-    return video_data
+    #return video_data
